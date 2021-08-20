e67db8b43ed35a7922f828dd88325527df99359a
maxfronek <mf@maxfronek.com>
2021-08-19 21:54:56 -0700

    Adding databse to git as API had only 100 questions

--- js/theAnswerIs.js
+++ js/theAnswerIs.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
         html += '</div>';
 
         $('#dictionary').append(html).fadeIn(); 
-    */
+
 
     function getClue() {
 
@@ -44,5 +44,74 @@ $(document).ready(function () {
     } // end getClue
 
     getClue();
+    */
+
+    var request = new XMLHttpRequest()
+
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', 'https://jservice.io/api/clues', true)
+
+    request.onload = function () {
+    // Begin accessing JSON data here
+    var clues = JSON.parse(this.response)
+
+    if (request.status >= 200 && request.status < 400) {
+        const randomClue = Math.floor(Math.random() * clues.length);    
+        console.log("Clue number: " + randomClue);
+
+        let clueQuestion = clues[randomClue].question,
+            clueAnswer = clues[randomClue].answer,
+            clueCat = clues[randomClue].category.title,
+            clueVal = clues[randomClue].value;
+        /*
+        console.log("In the category " + clueCat);    
+        console.log(clueQuestion);
+        console.log("What is: " + clueAnswer);
+        console.log("You've earned: " + clueVal);
+        */
+
+        let displayClue = document.getElementById('clue'),
+            displayCat = document.getElementById('category'),
+            displayValue = document.getElementById('value');
+        
+           $(displayClue).html(clueQuestion);
+           $(displayCat).html(clueCat);
+           $(displayValue).html("$" + clueVal);
+
+           let button = document.getElementById('theAnswerIs');
+
+
+   
+         $(button).on("click", function showAnswer() {
+            const answer = document.createElement('span');
+            answer.textContent = "What is " + clueAnswer + "?";
+            $(displayClue).append(answer);
+            $(button).remove();
+
+            const nextQuestion = document.createElement('button');
+            $(nextQuestion).attr("id", "nextQuestion");
+            nextQuestion.textContent = "Next Question";
+
+            $(displayClue).after(nextQuestion);
+
+
+        $(nextQuestion).on("click", function reloadPage() {
+            location.reload();
+        });
+
+        }); // end button on click
+
+        
+
+      } else {
+        console.log('API is inaccessible')
+      }
+
+ 
+
+    } // end request.onload
+
+    // Send request
+    request.send()
 
 }); // end document.ready
