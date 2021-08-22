e835f862f198a83f79ffd1f494d27fc90e0355f2
Max Fronek <mf@maxfronek.com>
2021-08-22 14:40:18 -0700

    Reconfigure for AJAX loading of clues

--- js/theAnswerIs.js
+++ js/theAnswerIs.js
@@ -10,72 +10,79 @@ $(document).ready(function () {
     // Begin accessing JSON data here
     var clues = JSON.parse(this.response)
 
-    if (request.status >= 200 && request.status < 400) {
+    function getClue() {
         const randomClue = Math.floor(Math.random() * clues.length),
-              loader = document.getElementById('loader');   
+        loader = document.getElementById('loader');   
         console.log("Clue number: " + randomClue);
 
-        let clueQuestion = clues[randomClue].question,
-            trimmedClueQuestion = clueQuestion.slice(1, -1);
-            clueAnswer = clues[randomClue].answer,
-            clueCat = clues[randomClue].category,
-            clueVal = clues[randomClue].value;
-        /*
-        console.log("In the category " + clueCat);    
-        console.log(clueQuestion);
-        console.log("What is: " + clueAnswer);
-        console.log("You've earned: " + clueVal);
-        */
+  let clueQuestion = clues[randomClue].question,
+      trimmedClueQuestion = clueQuestion.slice(1, -1);
+      clueAnswer = clues[randomClue].answer,
+      clueCat = clues[randomClue].category,
+      clueVal = clues[randomClue].value;
+  /*
+  console.log("In the category " + clueCat);    
+  console.log(clueQuestion);
+  console.log("What is: " + clueAnswer);
+  console.log("You've earned: " + clueVal);
+  */
 
-        let displayClue = document.getElementById('clue'),
-            displayCat = document.getElementById('category'),
-            displayValue = document.getElementById('value'),
-            displayAnswer = document.getElementById('answer'),
-            trimmedAnswer = clueAnswer.replace(/\\|/g,'');
+  let displayClue = document.getElementById('clue'),
+      displayCat = document.getElementById('category'),
+      displayValue = document.getElementById('value'),
+      displayAnswer = document.getElementById('answer'),
+      trimmedAnswer = clueAnswer.replace(/\\|/g,'');
 
-           $(displayClue).html(trimmedClueQuestion);
-           $(displayClue).addClass('loaded')
-           $(displayCat).html(clueCat);
-           $(displayValue).html(clueVal);
-           $(displayAnswer).html("What is " + trimmedAnswer + "?");
+     $(displayClue).html(trimmedClueQuestion);
+     $(displayClue).addClass('loaded')
+     $(displayCat).html(clueCat);
+     $(displayValue).html(clueVal);
+     $(displayAnswer).html("What is " + trimmedAnswer + "?");
 
-        const answerButton = document.createElement('button');
-           $(answerButton).attr("id", "theAnswerIs");
-             answerButton.textContent = 'The Answer Is...';
-           $('footer').html(answerButton);
-
-
-           $(loader).fadeOut("fast", function(){ });
-
-        let showAnswer = document.getElementById('theAnswerIs');
-
-         $(showAnswer).on("click", function showAnswerFunction() { 
-            $(displayClue).addClass('faded');
-            setTimeout (function() {
-                $(displayAnswer).addClass('loaded'); 
-            }, 150);
-
-            $(showAnswer).remove();
-
-            const nextQuestion = document.createElement('button');
-            $(nextQuestion).attr("id", "nextQuestion");
-            // nextQuestion.textContent = "Next Question";
-            // $(nextQuestion).html()
-            nextQuestion.innerHTML = '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 380.59 128.64"><polygon points="318.18 1.91 311.11 8.98 361.44 59.32 0 59.32 0 69.32 361.44 69.32 311.11 119.66 318.18 126.73 380.59 64.32 318.18 1.91"/></svg>'
-            $('footer').html(nextQuestion);
+  const answerButton = document.createElement('button');
+     $(answerButton).attr("id", "theAnswerIs");
+       answerButton.textContent = 'The Answer Is...';
+     $('footer').html(answerButton);
 
 
-            $(nextQuestion).on("click", function reloadPage() {
-                $('.container').fadeOut( "fast", function(){});
-                setTimeout(function() {
-                    location.reload();
-                }, 150);
+     $(loader).fadeOut("fast", function(){ });
 
-            });
+  let showAnswer = document.getElementById('theAnswerIs');
 
-        }); // end button on click
+   $(showAnswer).on("click", function showAnswerFunction() { 
+      $(displayClue).addClass('faded');
+      setTimeout (function() {
+          $(displayAnswer).addClass('loaded'); 
+      }, 150);
 
+      $(showAnswer).remove();
+
+      const nextQuestion = document.createElement('button');
+      $(nextQuestion).attr("id", "nextQuestion");
+      // nextQuestion.textContent = "Next Question";
+      // $(nextQuestion).html()
+      nextQuestion.innerHTML = '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 380.59 128.64"><polygon points="318.18 1.91 311.11 8.98 361.44 59.32 0 59.32 0 69.32 361.44 69.32 311.11 119.66 318.18 126.73 380.59 64.32 318.18 1.91"/></svg>'
+      $('footer').html(nextQuestion);
+
+
+      $(nextQuestion).on("click", function reloadPage() {
+          $('.container').fadeOut( "fast", function(){});
+          setTimeout(function() {
+                  $('.container__wrap').load('index.html .container', function() {
+                      getClue();
+                  });
+          }, 150);
+
+      });
+
+  }); // end button on click
+
+  
+    }
+
+    if (request.status >= 200 && request.status < 400) {
         
+        getClue();
 
       } else {
         console.log('API is inaccessible')
